b49ea11f9187af0a165e687d4f49c8dfc6a9164f
bouzuya <m@bouzuya.net>
2024-09-05 22:47:36 +0900

    Fix behavior of list subcommand when the directory is empty

--- src/command/list.rs
+++ src/command/list.rs
@@ -21,30 +21,32 @@ pub fn execute() -> anyhow::Result<()> {
         .join(now.format("%d").to_string());
 
     let mut lines = BTreeSet::new();
-    for dir_entry in fs::read_dir(today_dir)? {
-        let dir_entry = dir_entry?;
-        let path = dir_entry.path();
-        if let Some(extension) = path.extension() {
-            if extension == "md" {
-                let id = path
-                    .file_stem()
-                    .context("no file_stem")?
-                    .to_str()
-                    .context("not UTF-8")?
-                    .to_string();
+    if today_dir.exists() {
+        for dir_entry in fs::read_dir(today_dir)? {
+            let dir_entry = dir_entry?;
+            let path = dir_entry.path();
+            if let Some(extension) = path.extension() {
+                if extension == "md" {
+                    let id = path
+                        .file_stem()
+                        .context("no file_stem")?
+                        .to_str()
+                        .context("not UTF-8")?
+                        .to_string();
 
-                let mut file = File::open(&path)?;
-                let mut buf = [0; 1024];
-                let _ = Read::read(&mut file, &mut buf)?;
-                let s = String::from_utf8_lossy(&buf);
-                let s = s.trim_end_matches(char::REPLACEMENT_CHARACTER);
-                let s = s.replace('\n', " ");
-                let s = s.chars().take(30).collect::<String>();
-                // YYYYMMDDTHHMMSSZ
-                // 12345678901234567...
-                // 78 - 17 = 61
-                // 61 / 2 ~= 30
-                lines.insert(format!("{} {}", id, s));
+                    let mut file = File::open(&path)?;
+                    let mut buf = [0; 1024];
+                    let _ = Read::read(&mut file, &mut buf)?;
+                    let s = String::from_utf8_lossy(&buf);
+                    let s = s.trim_end_matches(char::REPLACEMENT_CHARACTER);
+                    let s = s.replace('\n', " ");
+                    let s = s.chars().take(30).collect::<String>();
+                    // YYYYMMDDTHHMMSSZ
+                    // 12345678901234567...
+                    // 78 - 17 = 61
+                    // 61 / 2 ~= 30
+                    lines.insert(format!("{} {}", id, s));
+                }
             }
         }
     }
